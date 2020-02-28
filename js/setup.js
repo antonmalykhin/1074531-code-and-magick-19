373aeb5a8cddaae726dac951371b361b6d6353ba
antonmalykhin <a.y.malykhin@gmail.com>
2020-02-29 01:20:58 +0300

    Реализует экспорт из модуля
    Добавялет функцию отображения отфильтрованных
    похожих персонажей
    Экспортирует функцию изменения похожих персонажей

--- js/setup.js
+++ js/setup.js
@@ -1,46 +1,76 @@
 'use strict';
-/**
- * Количество похожих персонажей для отображения
- * @constant
- * @type {number}
- */
-var WIZARDS_QUANTITY = 4;
 
-/**
- * Функция генерации разметки окна персонажа
- * @param {array} personages - Список свойств персонажей
- * @param {array} personagesQuantity - Количество отображаемых персонажей
- */
-var generatePersonageMarkup = function (personages, personagesQuantity) {
-  var similarListElement = document.querySelector('.setup-similar-list');
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+(function () {
 
-  var fragment = document.createDocumentFragment();
+  /**
+   * Количество похожих персонажей для отображения
+   * @constant
+   * @type {number}
+   */
+  // var WIZARDS_QUANTITY = 4;
 
-  for (var i = 0; i < personagesQuantity; i++) {
-    var wizardElement = similarWizardTemplate.cloneNode(true);
-    var wizardName = wizardElement.querySelector('.setup-similar-label');
-    var wizardCoat = wizardElement.querySelector('.wizard-coat');
-    var wizardEyes = wizardElement.querySelector('.wizard-eyes');
+  /**
+   * Функция генерации разметки окна персонажа
+   * @param {array} personages - Список свойств персонажей
+   * @param {array} personagesQuantity - Количество отображаемых персонажей
+   */
+  var generatePersonageMarkup = function (personages) {
+    var similarListElement = document.querySelector('.setup-similar-list');
+    var similarElements = similarListElement.querySelectorAll('.setup-similar-item');
 
-    wizardName.innerText = personages[i].name;
-    wizardCoat.setAttribute('style', 'fill: ' + personages[i].colorCoat + ';');
-    wizardEyes.setAttribute('style', 'fill: ' + personages[i].colorEyes + ';');
+    if (similarElements) {
+      similarElements.forEach(function (element) {
+        similarListElement.removeChild(element);
+      });
+    }
 
-    fragment.appendChild(wizardElement);
-  }
+    var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-  similarListElement.appendChild(fragment);
-};
+    var fragment = document.createDocumentFragment();
 
-/**
- * Функция успешной загрузки данных похожих персонажей
- * @param {Array} wizards - список похожих персонажей
- */
-var onSuccessLoad = function (wizards) {
-  generatePersonageMarkup(wizards, WIZARDS_QUANTITY);
-  document.querySelector('.setup-similar').classList.remove('hidden');
-};
+    for (var i = 0; i < personages.length; i++) {
+      var wizardElement = similarWizardTemplate.cloneNode(true);
+      var wizardName = wizardElement.querySelector('.setup-similar-label');
+      var wizardCoat = wizardElement.querySelector('.wizard-coat');
+      var wizardEyes = wizardElement.querySelector('.wizard-eyes');
 
-window.backend.load(onSuccessLoad, window.utils.onErrorLoad);
+      wizardName.innerText = personages[i].name;
+      wizardCoat.setAttribute('style', 'fill: ' + personages[i].colorCoat + ';');
+      wizardEyes.setAttribute('style', 'fill: ' + personages[i].colorEyes + ';');
 
+      fragment.appendChild(wizardElement);
+    }
+
+    similarListElement.appendChild(fragment);
+  };
+
+  var tempWizards = [];
+
+  var changeWizards = function (coat) {
+
+    var filteredWizardsCoat = tempWizards.filter(function (wiz) {
+      return wiz.colorCoat === coat;
+    });
+
+
+    generatePersonageMarkup(filteredWizardsCoat);
+  };
+
+  /**
+   * Функция успешной загрузки данных похожих персонажей
+   * @param {Array} wizards - список похожих персонажей
+   */
+  var onSuccessLoad = function (wizards) {
+    tempWizards = wizards;
+
+    changeWizards(window.personageCustomization.coatColor);
+    document.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  window.backend.load(onSuccessLoad, window.utils.onErrorLoad);
+
+  window.setup = {
+    changeWizards: changeWizards
+  };
+
+})();
